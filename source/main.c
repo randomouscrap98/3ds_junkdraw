c85eab273a75ada1989023ddf55bb542d2ba4499
Carlos Sanchez <randomouscrap98@aol.com>
2021-05-26 19:00:32 -0400

    Figured out rgba5551 color format

--- source/main.c
+++ source/main.c
@@ -9,30 +9,48 @@
 
 #define SCREENWIDTH 320
 #define SCREENHEIGHT 240
-#define PAGEWIDTH 512
-#define PAGEHEIGHT 512
+
+#define PAGEWIDTH 1024
+#define PAGEHEIGHT 1024
+#define PAGEFORMAT GPU_RGBA5551
+#define PAGE_EDGEBUF 8
+#define PAGECOUNT 2
+
 #define CPAD_DEADZONE 40
 #define CPAD_THEORETICALMAX 160
 #define CPAD_PAGECONST 1
 #define CPAD_PAGEMULT 0.02f
 #define CPAD_PAGECURVE 3.2f
 
-//Return the new page position given the existing position and the circlepad input
-float calc_pagepos(circlePosition pos, float existing_pos)
+//#define DEBUG_COORD
+
+
+//Generic page difference using cpad values, return the new page position given 
+//the existing position and the circlepad input
+float calc_pagepos(s16 d, float existing_pos, u32 max_pos)
 {
-   u16 cpadmag = abs(pos.dy);
+   u16 cpadmag = abs(d);
 
    if(cpadmag > CPAD_DEADZONE)
    {
-      return C2D_Clamp(existing_pos + (pos.dy < 0 ? 1 : -1) * 
+      return C2D_Clamp(existing_pos + (d < 0 ? -1 : 1) * 
             (CPAD_PAGECONST + pow(cpadmag * CPAD_PAGEMULT, CPAD_PAGECURVE)), 
-            0, PAGEHEIGHT - SCREENHEIGHT);
+            0, max_pos);
    }
    else
    {
       return existing_pos;
    }
 }
+
+float calc_pagepos_x(circlePosition pos, float existing_pos) {
+   return calc_pagepos(pos.dx, existing_pos, PAGEWIDTH - SCREENWIDTH);
+}
+float calc_pagepos_y(circlePosition pos, float existing_pos) {
+   return calc_pagepos(-pos.dy, existing_pos, PAGEHEIGHT - SCREENHEIGHT);
+}
+
+
 
 struct PageData {
    Tex3DS_SubTexture subtex;     //Simple structures
@@ -44,17 +62,34 @@ struct PageData {
 void create_page(struct PageData * result, Tex3DS_SubTexture subtex)
 {
    result->subtex = subtex;
-   C3D_TexInitVRAM(&(result->texture), subtex.width, subtex.height, GPU_RGBA5551);
+   C3D_TexInitVRAM(&(result->texture), subtex.width, subtex.height, PAGEFORMAT);
    result->target = C3D_RenderTargetCreateFromTex(&(result->texture), GPU_TEXFACE_2D, 0, -1);
    result->image.tex = &(result->texture);
    result->image.subtex = &(result->subtex);
 }
 
-void clear_page(struct PageData page)
+void delete_page(struct PageData page)
 {
    C3D_RenderTargetDelete(page.target);
    C3D_TexDelete(&page.texture);
 }
+
+u32 full_to_half(u32 val)
+{
+   //Format: 0xAABBGGRR
+   //Half  : 0bGGBBBBBARRRRRGGGxxxxxxxxxxxxxxxx
+   u8 green = (val & 0x0000FF00) >> 11; //crush last three bits
+
+   return 
+      (
+         (val & 0xFF000000 ? 0x0100 : 0) +   //Alpha is lowest bit on upper byte
+         (val & 0x000000F8) +                //Red is slightly nice because it's already in the right position
+         ((green & 0x1c) >> 2) + ((green & 0x03) << 14) + //Green is split between bytes
+         ((val & 0x00F80000) >> 10)          //Blue is just shifted and crushed
+      ) << 16; //first 2 bytes are trash
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -66,8 +101,14 @@ int main(int argc, char** argv)
    consoleInit(GFX_TOP, NULL);
    C3D_RenderTarget* screen = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
 
-   const u32 initial_color = C2D_Color32(0,0,0,0);
-   const u32 bg_color = C2D_Color32(255,255,255,255);
+   //const u32 initial_color = 0xffff;//C2D_Color32f(0.5f, 0.5f, 0.5f, 1.0f);
+   //weird byte order? 16 bits of color are at top
+   //const u32 initial_color = full_to_half(C2D_Color32(0x12,0x34,0x56,0x78));
+   const u32 initial_color = full_to_half(C2D_Color32(128,128,128,255));
+      //= 0x01f80000; //C2D_Color32(0,0,200,255);
+      //1 + 31 + (16 << 5) + (16 << 10);//C2D_Color32f(0.5f,1.0f,1.0f,1.0f);
+   //const u32 bg_color = C2D_Color32(255,255,255,255);
+   const u32 bg_color = C2D_Color32f(0.0,0.0,0.00,1.0);
 
    const u32 color_a = C2D_Color32f(1,0,0,1.0f);
    const u32 color_b = C2D_Color32f(1,1,0,1.0f);
@@ -81,9 +122,10 @@ int main(int argc, char** argv)
       0.0f, 1.0f, 1.0f, 0.0f
    };
 
-   struct PageData frontpg, backpg; 
-   create_page(&frontpg, subtex); 
-   create_page(&backpg, subtex);
+   struct PageData pages[PAGECOUNT]; // frontpg, backpg; 
+
+   for(int i = 0; i < PAGECOUNT; i++)
+      create_page(pages + i, subtex);
 
    touchPosition current_touch;
    touchPosition last_touch = current_touch; //Why? compiler warning shush
@@ -92,14 +134,16 @@ int main(int argc, char** argv)
    bool page_initialized = false;
    u32 current_frame = 0;
    u32 start_frame = 0;
-   float page_pos = 0;
-
-   C3D_RenderTarget * target = frontpg.target;
+   float page_pos_y = 0;
+   float page_pos_x = 0;
+   u16 width = 2;
+   u16 current_page = PAGECOUNT - 1; //Always go on the top page
 
    printf("Press ABXY to change color\n");
    printf("Press SELECT to change layers\n");
    printf("C-pad to scroll up/down\n");
    printf("\nPress START to quit.\n");
+   printf("STARTING COLOR: %08x\n", initial_color);
 
 
    while(aptMainLoop())
@@ -119,16 +163,8 @@ int main(int argc, char** argv)
       
       if(kDown & KEY_SELECT)
       {
-         if(target == frontpg.target)
-         {
-            target = backpg.target;
-            printf("Changing to back layer\n");
-         }
-         else
-         {
-            target = frontpg.target;
-            printf("Changing to front layer\n");
-         }
+         current_page = (current_page + 1) % PAGECOUNT;
+         printf("Changing to layer %d\n", current_page);
       }
 
       hidTouchRead(&current_touch);
@@ -147,46 +183,60 @@ int main(int argc, char** argv)
 
       circlePosition pos;
 		hidCircleRead(&pos);
-      page_pos = calc_pagepos(pos, page_pos);
+      page_pos_x = calc_pagepos_x(pos, page_pos_x);
+      page_pos_y = calc_pagepos_y(pos, page_pos_y);
 
       // Render the scene
       C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 
       if(!page_initialized)
       {
-         C2D_TargetClear(frontpg.target, initial_color); 
-         C2D_TargetClear(backpg.target, initial_color);
+         for(int i = 0; i < PAGECOUNT; i++)
+            C2D_TargetClear(pages[i].target, initial_color); 
          page_initialized = true;
       }
 
       if(touching)
       {
-         C2D_SceneBegin(target);
+         C2D_SceneBegin(pages[current_page].target);
+
+         u16 width_ofs = width / 2;
+         float old_x = C2D_Clamp(last_touch.px + page_pos_x, PAGE_EDGEBUF + width_ofs, PAGEWIDTH - 1);
+         float old_y = C2D_Clamp(last_touch.py + page_pos_y, PAGE_EDGEBUF + width_ofs, PAGEHEIGHT - 1);
+         float real_x = C2D_Clamp(current_touch.px + page_pos_x, PAGE_EDGEBUF + width_ofs, PAGEWIDTH - 1);
+         float real_y = C2D_Clamp(current_touch.py + page_pos_y, PAGE_EDGEBUF + width_ofs, PAGEHEIGHT - 1);
+
+#ifdef DEBUGCOORD
+         printf("%d,%d: %.1f,%.1f  %.1f,%.1f\n", 
+               current_touch.px, current_touch.py, 
+               page_pos_x, page_pos_y, real_x, real_y);
+#endif
 
          //Calling JUST C2D_DrawLine (like I want) produces no output. Calling
          //C2DDrawRectSolid beforehand makes it all work.
          //if(start_frame == current_frame)
          //{
-            C2D_DrawRectSolid(current_touch.px - 1, current_touch.py - 1, 0.5f, 2, 2, *color_selected);
+         //C2D_DrawRectSolid(0,0,0.5f,1,1,*color_selected);
+               //real_x - 1, real_y - 1, 0.5f, 2, 2, *color_selected);
+         C2D_DrawRectSolid(real_x - width_ofs, real_y - width_ofs, 0.5f, width, width, *color_selected);
          //}
 
-         C2D_DrawLine(last_touch.px, last_touch.py + page_pos, *color_selected,
-               current_touch.px, current_touch.py + page_pos, *color_selected, 
-               2, 0.5f);
+         C2D_DrawLine(old_x, old_y, *color_selected, real_x, real_y, *color_selected, 
+               width, 0.5f);
       }
 
       C2D_TargetClear(screen, bg_color);
       C2D_SceneBegin(screen);
-      C2D_DrawImageAt(backpg.image, 0, -page_pos, 0.5f, NULL, 1.0f, 1.0f);
-      C2D_DrawImageAt(frontpg.image, 0, -page_pos, 0.5f, NULL, 1.0f, 1.0f);
+      for(int i = 0; i < PAGECOUNT; i++)
+         C2D_DrawImageAt(pages[i].image, -page_pos_x, -page_pos_y, 0.5f, NULL, 1.0f, 1.0f);
       C3D_FrameEnd(0);
 
       last_touch = current_touch;
       current_frame++;
    }
 
-   clear_page(frontpg);
-   clear_page(backpg);
+   for(int i = 0; i < PAGECOUNT; i++)
+      delete_page(pages[i]);
 
    C2D_Fini();
    C3D_Fini();
