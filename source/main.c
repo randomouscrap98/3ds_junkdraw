0196dddd398db418849cb3f91b32082ba58fcb9a
Carlos Sanchez <randomouscrap98@aol.com>
2021-05-27 04:34:42 -0400

    Love stack exceptions

--- source/main.c
+++ source/main.c
@@ -22,6 +22,13 @@
 #define CPAD_PAGEMULT 0.02f
 #define CPAD_PAGECURVE 3.2f
 
+#define MAX_STROKE_LINES 5000
+
+#define TOOL_PENCIL 0
+#define TOOL_ERASER 0
+
+#define LINESTYLE_STROKE 0
+
 //#define DEBUG_COORD
 #define DEBUG_RUNTESTS
 
@@ -31,6 +38,53 @@
 //   MUST have the proper 16 bit format....
 // - ClearTarget with a transparent color seems to make the color stick using
 //   DrawLine unless a DrawRect (or perhaps other) call is performed.
+
+
+// -- SIMPLE UTILS --
+
+u32 full_to_half(u32 val)
+{
+   //Format: 0b AAAAAAAA BBBBBBBB GGGGGGGG RRRRRRRR
+   //Half  : 0b GGBBBBBA RRRRRGGG 00000000 00000000
+   u8 green = (val & 0x0000FF00) >> 11; //crush last three bits
+
+   return 
+      (
+         (val & 0xFF000000 ? 0x0100 : 0) |   //Alpha is lowest bit on upper byte
+         (val & 0x000000F8) |                //Red is slightly nice because it's already in the right position
+         ((green & 0x1c) >> 2) | ((green & 0x03) << 14) | //Green is split between bytes
+         ((val & 0x00F80000) >> 10)          //Blue is just shifted and crushed
+      ) << 16; //first 2 bytes are trash
+}
+
+u16 full_to_truehalf(u32 val)
+{
+   return full_to_half(val) >> 16;
+}
+
+u32 half_to_full(u16 val)
+{
+   //Half : 0b                   GGBBBBBA RRRRRGGG
+   //Full : 0b AAAAAAAA BBBBBBBB GGGGGGGG RRRRRRRR
+   return
+      (
+         (val & 0x0100 ? 0xFF000000 : 0)  |  //Alpha always 255 or 0
+         ((val & 0x3E00) << 10) |            //Blue just shift a lot
+         ((val & 0x0007) << 13) | ((val & 0xc000) >> 3) |  //Green is split
+         (val & 0x00f8)                      //Red is easy, already in the right place
+      );
+}
+
+
+
+// -- SCREEN UTILS? --
+
+struct ScreenModifier
+{
+   float ofs_x;
+   float ofs_y;
+   float zoom;
+};
 
 //Generic page difference using cpad values, return the new page position given 
 //the existing position and the circlepad input
@@ -50,28 +104,58 @@ float calc_pagepos(s16 d, float existing_pos, u32 max_pos)
    }
 }
 
-float calc_pagepos_x(circlePosition pos, float existing_pos) {
-   return calc_pagepos(pos.dx, existing_pos, PAGEWIDTH - SCREENWIDTH);
+void update_screenmodifier(struct ScreenModifier * mod, circlePosition pos)
+{
+   mod->ofs_x = calc_pagepos(pos.dx, mod->ofs_x, PAGEWIDTH - SCREENWIDTH);
+   mod->ofs_y = calc_pagepos(pos.dy, mod->ofs_y, PAGEHEIGHT - SCREENHEIGHT);
 }
-float calc_pagepos_y(circlePosition pos, float existing_pos) {
-   return calc_pagepos(-pos.dy, existing_pos, PAGEHEIGHT - SCREENHEIGHT);
-}
 
 
 
+// -- DRAWING UTILS --
+
+//A line doesn't contain all the data needed to draw itself. That would be a
+//line package
 struct SimpleLine {
-   u8 layer;
-   u16 color;
-   u8 width;
-   u16 x1;
-   u16 y1;
-   u16 x2;
-   u16 y2;
-   bool rectangle;
+   u16 x1, y1, x2, y2;
 };
 
+struct LinePackage {
+   u8 style;
+   u16 color;
+   u8 layer;
+   u8 width;
+   struct SimpleLine * lines;
+   u16 line_count;
+};
+
+//Assumes you're already on the appropriate page you want and all that
+void draw_lines(const struct LinePackage * linepack, const struct ScreenModifier * mod)
+{
+   u16 width_ofs = linepack->width / 2;
+   u32 color = half_to_full(linepack->color);
+   struct SimpleLine * lines = linepack->lines;
+
+   for(int i = 0; i < linepack->line_count; i++)
+   {
+      float old_x = C2D_Clamp(lines[i].x1 + mod->ofs_x, PAGE_EDGEBUF + width_ofs, PAGEWIDTH - 1);
+      float old_y = C2D_Clamp(lines[i].y1 + mod->ofs_y, PAGE_EDGEBUF + width_ofs, PAGEHEIGHT - 1);
+      float real_x = C2D_Clamp(lines[i].x2 + mod->ofs_x, PAGE_EDGEBUF + width_ofs, PAGEWIDTH - 1);
+      float real_y = C2D_Clamp(lines[i].y2 + mod->ofs_y, PAGE_EDGEBUF + width_ofs, PAGEHEIGHT - 1);
+      //For some reason, I can't just draw a line, because the system won't draw
+      //it if the target is cleared with transparency. But drawing a rect first
+      //fixes that, so... guess that's just part of the style of the thing.
+      C2D_DrawRectSolid(real_x - width_ofs, real_y - width_ofs, 0.5f, 
+            linepack->width, linepack->width, color);
+      C2D_DrawLine(old_x, old_y, color, real_x, real_y, color, linepack->width, 0.5f);
+      //TODO: doesn't take into account the type yet! Some types (like
+      //rectangle) draw something other than lines!
+   }
+}
 
 
+
+// -- LAYER UTILS --
 
 struct PageData {
    Tex3DS_SubTexture subtex;     //Simple structures
@@ -97,42 +181,8 @@ void delete_page(struct PageData page)
 
 
 
-u32 full_to_half(u32 val)
-{
-   //Format: 0b AAAAAAAA BBBBBBBB GGGGGGGG RRRRRRRR
-   //Half  : 0b GGBBBBBA RRRRRGGG 00000000 00000000
-   u8 green = (val & 0x0000FF00) >> 11; //crush last three bits
 
-   return 
-      (
-         (val & 0xFF000000 ? 0x0100 : 0) |   //Alpha is lowest bit on upper byte
-         (val & 0x000000F8) |                //Red is slightly nice because it's already in the right position
-         ((green & 0x1c) >> 2) | ((green & 0x03) << 14) | //Green is split between bytes
-         ((val & 0x00F80000) >> 10)          //Blue is just shifted and crushed
-      ) << 16; //first 2 bytes are trash
-}
-
-u32 half_to_full(u16 val)
-{
-   //Half : 0b                   GGBBBBBA RRRRRGGG
-   //Full : 0b AAAAAAAA BBBBBBBB GGGGGGGG RRRRRRRR
-   return
-      (
-         (val & 0x0100 ? 0xFF000000 : 0)  |  //Alpha always 255 or 0
-         ((val & 0x3E00) << 10) |            //Blue just shift a lot
-         ((val & 0x0007) << 13) | ((val & 0xc000) >> 3) |  //Green is split
-         (val & 0x00f8)                      //Red is easy, already in the right place
-      );
-}
-
-
-
-void run_tests()
-{
-   printf("Running tests; only errors will be displayed\n");
-   if(test_transparenthalftofull()) return; 
-   printf("\nAll tests passed\n");
-}
+// -- TESTS --
 
 int test_transparenthalftofull()
 {
@@ -144,7 +194,7 @@ int test_transparenthalftofull()
       if((col & 0xFFF) == 0xFFF) printf(".");
       if((col << 16) != half)
       {
-         printf("ERR: Expected %08x, got %08x, full: %08x\n", col << 16, half, full);
+         printf("ERR: Expected %08lx, got %08lx, full: %08lx\n", col << 16, half, full);
          return 1;
       }
    }
@@ -152,6 +202,17 @@ int test_transparenthalftofull()
    return 0;
 }
 
+void run_tests()
+{
+   printf("Running tests; only errors will be displayed\n");
+   if(test_transparenthalftofull()) return; 
+   printf("\nAll tests passed\n");
+}
+
+
+
+
+// -- MAIN, OFC --
 
 int main(int argc, char** argv)
 {
@@ -164,44 +225,59 @@ int main(int argc, char** argv)
    C3D_RenderTarget* screen = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
 
    //weird byte order? 16 bits of color are at top
-   const u32 initial_color = full_to_half(C2D_Color32(128,128,128,255));
-   //const u32 bg_color = C2D_Color32(255,255,255,255);
-   const u32 bg_color = C2D_Color32f(0.0,0.0,0.00,1.0);
+   const u32 layer_color = full_to_half(C2D_Color32(0,0,0,0)); //128,128,128,255));
+   const u32 bg_color = C2D_Color32(255,255,255,255);
+   //const u32 bg_color = C2D_Color32f(0.0,0.0,0.00,1.0);
 
    const u32 color_a = C2D_Color32f(1,0,0,1.0f);
    const u32 color_b = C2D_Color32f(1,1,0,1.0f);
    const u32 color_x = C2D_Color32f(0,0,1,1.0f);
    const u32 color_y = C2D_Color32f(0,1,0,1.0f);
 
-   const u32* color_selected = &color_a;
+   //const u32* color_selected = &color_a;
 
    const Tex3DS_SubTexture subtex = {
       PAGEWIDTH, PAGEHEIGHT,
       0.0f, 1.0f, 1.0f, 0.0f
    };
 
-   struct PageData pages[PAGECOUNT]; // frontpg, backpg; 
+   struct PageData pages[PAGECOUNT];
 
    for(int i = 0; i < PAGECOUNT; i++)
       create_page(pages + i, subtex);
 
+   struct ScreenModifier screen_mod = {0,0,0}; 
+
    touchPosition current_touch;
    touchPosition last_touch = current_touch; //Why? compiler warning shush
-   touchPosition start_touch;
+   //touchPosition start_touch;
    bool touching = false;
    bool page_initialized = false;
    u32 current_frame = 0;
    u32 start_frame = 0;
-   float page_pos_y = 0;
-   float page_pos_x = 0;
-   u16 width = 2;
-   u16 current_page = PAGECOUNT - 1; //Always go on the top page
+   u32 end_frame = 0;
+   //u16 width = 2;
+   //u16 current_page = PAGECOUNT - 1; //Always go on the top page
+
+   //u8 tool = 0;
+
+   struct LinePackage pending;
+   struct SimpleLine pending_lines[MAX_STROKE_LINES];
+   pending.lines = pending_lines; //Use the stack for my pending stroke
+   pending.line_count = 0;
+   pending.style = LINESTYLE_STROKE;
+   pending.width = 2;
+   pending.layer = PAGECOUNT - 1; //Always start on the top page
+   pending.color = full_to_truehalf(color_a);
+
+   //u16 pending_count = TOOL_PENCIL;
+   //u8 pending_style = LINESTYLE_STROKE;
 
    printf("Press ABXY to change color\n");
    printf("Press SELECT to change layers\n");
    printf("C-pad to scroll up/down\n");
    printf("\nPress START to quit.\n");
-   printf("STARTING COLOR: %08x\n", initial_color);
+   printf("STARTING COLOR: %08lx\n", layer_color);
 
 #ifdef DEBUG_RUNTESTS
    run_tests();
@@ -217,35 +293,35 @@ int main(int argc, char** argv)
       // Respond to user input
       if(kDown & KEY_START) break;
 
-      if(kDown & KEY_A) color_selected = &color_a;
-      else if(kDown & KEY_B) color_selected = &color_b;
-      else if(kDown & KEY_X) color_selected = &color_x;
-      else if(kDown & KEY_Y) color_selected = &color_y;
+      if(kDown & KEY_A) pending.color = full_to_truehalf(color_a);
+      else if(kDown & KEY_B) pending.color = full_to_truehalf(color_b);
+      else if(kDown & KEY_X) pending.color = full_to_truehalf(color_x);
+      else if(kDown & KEY_Y) pending.color = full_to_truehalf(color_y);
       
       if(kDown & KEY_SELECT)
       {
-         current_page = (current_page + 1) % PAGECOUNT;
-         printf("Changing to layer %d\n", current_page);
+         pending.layer = (pending.layer + 1) % PAGECOUNT;
+         //current_page = (current_page + 1) % PAGECOUNT;
+         printf("Changing to layer %d\n", pending.layer);
       }
 
       hidTouchRead(&current_touch);
 
       if(kDown & KEY_TOUCH) {
          start_frame = current_frame;
-         start_touch = current_touch;
+         //start_touch = current_touch;
          //Just throw away last touch from last time, we don't care
          last_touch = current_touch;
       }
       if(kUp & KEY_TOUCH) {
-         //Nothing yet
+         end_frame = current_frame;
       }
 
       touching = (kHeld & KEY_TOUCH) > 0;
 
       circlePosition pos;
 		hidCircleRead(&pos);
-      page_pos_x = calc_pagepos_x(pos, page_pos_x);
-      page_pos_y = calc_pagepos_y(pos, page_pos_y);
+      update_screenmodifier(&screen_mod, pos);
 
       // Render the scene
       C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
@@ -253,25 +329,42 @@ int main(int argc, char** argv)
       if(!page_initialized)
       {
          for(int i = 0; i < PAGECOUNT; i++)
-            C2D_TargetClear(pages[i].target, initial_color); 
+            C2D_TargetClear(pages[i].target, layer_color); 
          page_initialized = true;
       }
 
       if(touching)
       {
-         C2D_SceneBegin(pages[current_page].target);
+         C2D_SceneBegin(pages[pending.layer].target);
 
-         u16 width_ofs = width / 2;
-         float old_x = C2D_Clamp(last_touch.px + page_pos_x, PAGE_EDGEBUF + width_ofs, PAGEWIDTH - 1);
-         float old_y = C2D_Clamp(last_touch.py + page_pos_y, PAGE_EDGEBUF + width_ofs, PAGEHEIGHT - 1);
-         float real_x = C2D_Clamp(current_touch.px + page_pos_x, PAGE_EDGEBUF + width_ofs, PAGEWIDTH - 1);
-         float real_y = C2D_Clamp(current_touch.py + page_pos_y, PAGE_EDGEBUF + width_ofs, PAGEHEIGHT - 1);
+         //This is for a stroke, do different things if we have different tools!
+         struct SimpleLine * line = &pending.lines[pending.line_count];
+         line->x1 = last_touch.px;
+         line->y1 = last_touch.py;
+         line->x2 = current_touch.px;
+         line->y2 = current_touch.py;
 
-#ifdef DEBUGCOORD
-         printf("%d,%d: %.1f,%.1f  %.1f,%.1f\n", 
-               current_touch.px, current_touch.py, 
-               page_pos_x, page_pos_y, real_x, real_y);
-#endif
+         pending.lines = line; //Force the pending line to only show the end
+         u16 oldcount = pending.line_count;
+         pending.line_count = 1;
+
+         //Draw ONLY the current line
+         //draw_lines(&pending, &screen_mod);
+         //Reset pending lines to proper thing
+         pending.lines = pending_lines;
+         pending.line_count = oldcount + 1;
+
+         //u16 width_ofs = width / 2;
+         //float old_x = C2D_Clamp(last_touch.px + page_pos_x, PAGE_EDGEBUF + width_ofs, PAGEWIDTH - 1);
+         //float old_y = C2D_Clamp(last_touch.py + page_pos_y, PAGE_EDGEBUF + width_ofs, PAGEHEIGHT - 1);
+         //float real_x = C2D_Clamp(current_touch.px + page_pos_x, PAGE_EDGEBUF + width_ofs, PAGEWIDTH - 1);
+         //float real_y = C2D_Clamp(current_touch.py + page_pos_y, PAGE_EDGEBUF + width_ofs, PAGEHEIGHT - 1);
+
+//#ifdef DEBUGCOORD
+//         printf("%d,%d: %.1f,%.1f  %.1f,%.1f\n", 
+//               current_touch.px, current_touch.py, 
+//               page_pos_x, page_pos_y, real_x, real_y);
+//#endif
 
          //Calling JUST C2D_DrawLine (like I want) produces no output. Calling
          //C2DDrawRectSolid beforehand makes it all work.
@@ -279,24 +372,27 @@ int main(int argc, char** argv)
          //{
          //C2D_DrawRectSolid(0,0,0.5f,1,1,*color_selected);
                //real_x - 1, real_y - 1, 0.5f, 2, 2, *color_selected);
-         C2D_DrawRectSolid(real_x - width_ofs, real_y - width_ofs, 0.5f, width, width, *color_selected);
+         //C2D_DrawRectSolid(real_x - width_ofs, real_y - width_ofs, 0.5f, width, width, *color_selected);
          //}
+         //C2D_DrawLine(old_x, old_y, *color_selected, real_x, real_y, *color_selected, width, 0.5f);
+      }
 
-         C2D_DrawLine(old_x, old_y, *color_selected, real_x, real_y, *color_selected, 
-               width, 0.5f);
+      //TODO: Here, you would NORMALLY save the line or whatever
+      if(end_frame == current_frame)
+      {
+         pending.line_count = 0;
       }
 
       C2D_TargetClear(screen, bg_color);
       C2D_SceneBegin(screen);
       for(int i = 0; i < PAGECOUNT; i++)
-         C2D_DrawImageAt(pages[i].image, -page_pos_x, -page_pos_y, 0.5f, NULL, 1.0f, 1.0f);
+         C2D_DrawImageAt(pages[i].image, -screen_mod.ofs_x, -screen_mod.ofs_y, 0.5f, NULL, 1.0f, 1.0f);
       C3D_FrameEnd(0);
 
       last_touch = current_touch;
       current_frame++;
    }
 
-END:
    for(int i = 0; i < PAGECOUNT; i++)
       delete_page(pages[i]);
 
