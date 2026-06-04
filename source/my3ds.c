8f99f667278e81264d131625ba1b5d645d237897
haloopdy <randomouscrap98@aol.com>
2026-06-04 13:16:00 -0400

    remove unused code

--- source/my3ds.c
+++
@@ -1,9 +0,0 @@
-#include "my3ds.h"
-
-bool myAptMainLoop() {
-    static bool running = true;
-    // This shortcuts calling aptMainLoop
-    if(running && !aptMainLoop())
-        running = false;
-    return running;
-}
--- source/my3ds.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HEADER_MY3DS
-#define __HEADER_MY3DS
-
-#include <3ds.h>
-
-bool myAptMainLoop();
-
-#endif
