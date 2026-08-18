c899f063353b053c94457366100223a0136302df
haloopdy <randomouscrap98@aol.com>
2026-08-18 01:16:39 -0400

    Initial logging + screen clear

--- source_app/main.c
+++ source_app/main.c
@@ -1,17 +1,59 @@
+#include "3ds/console.h"
 #include "utils.h"
+#include "littlelogbox.h"
 
 #include <3ds.h>
 #include <citro3d.h>
 #include <citro2d.h>
 
+u32 __stacksize__ = 512 * 1024;
+
 #define BREPEAT_DELAY 20
 #define BREPEAT_INTERVAL 7
 #define MAX_FILENAME 64
+
+// Console crap
+#define UI_CONSOLE_LOGTOP       20
+#define UI_CONSOLE_LOGHEIGHT    8
+#define UI_CONSOLE_LOGCOLOR     CONSOLE_ESC(30;1m)
+
+// NOTE: for now, the log can't even scroll, so no need to make it large
+#define MAX_LOGMESSAGES 30
+#define MAX_LOGMSGLENGTH 100
 
 #define SCROLL_WIDTH 3
 #define SCREEN_COLOR C2D_Color32(90, 90, 90, 255)
 #define SCROLL_BG C2D_Color32f(0.8, 0.8, 0.8, 1)
 #define SCROLL_BAR C2D_Color32f(0.5, 0.5, 0.5, 1)
+
+// ==========================================
+//                Logging
+// ==========================================
+
+tui_logbox logbox;
+
+void LOGERR(const char * fmt, ...) { TUILOGBOX_LOG_INNER(&logbox, "E", fmt) }
+void LOGWRN(const char * fmt, ...) { TUILOGBOX_LOG_INNER(&logbox, "W", fmt) }
+void LOGINF(const char * fmt, ...) { TUILOGBOX_LOG_INNER(&logbox, "I", fmt) }
+void LOGDBG(const char * fmt, ...) { TUILOGBOX_LOG_INNER(&logbox, "D", fmt) }
+void LOGTRC(const char * fmt, ...) { TUILOGBOX_LOG_INNER(&logbox, "T", fmt) }
+
+// ==========================================
+//               Rendering
+// ==========================================
+
+void ui_render_logbox(tui_logbox * lb) {
+  char out[51]; // Just wide enough for the screen + null
+  for(int i = 0; i < UI_CONSOLE_LOGHEIGHT; i++) {
+    tui_logbox_renderline(lb, out, 50, UI_CONSOLE_LOGHEIGHT, i);
+    printf(CONSOLE_ESC(%d;%dH) UI_CONSOLE_LOGCOLOR "%s",
+           UI_CONSOLE_LOGTOP + i, 0, out);
+  }
+}
+
+// ==========================================
+//                  Main
+// ==========================================
 
 bool isn3ds() {
   bool isn3ds = false;
@@ -23,13 +65,13 @@ bool isn3ds() {
   return false;
 }
 
-
 int main() {
   gfxInitDefault();
   hidSetRepeatParameters(BREPEAT_DELAY, BREPEAT_INTERVAL);
 
-  // Enable the higher clock speed on New 3DS
-  osSetSpeedupEnable(true);
+  // Set this up IMMEDIATELY
+  if(tui_logbox_init(&logbox, MAX_LOGMESSAGES, MAX_LOGMSGLENGTH)) return 1;
+  tui_logbox_unit_t logbox_lasthead = logbox.head;
 
   C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
   C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
@@ -41,8 +83,8 @@ int main() {
 
   if(isn3ds()) {
     LOGDBG("New 3ds detected");
+    osSetSpeedupEnable(true);
   } 
-
 
   char save_filename[MAX_FILENAME];
 
@@ -79,6 +121,11 @@ int main() {
     C2D_TargetClear(screen, SCREEN_COLOR);
     C2D_SceneBegin(screen);
 
+    if(logbox.head != logbox_lasthead) {
+      ui_render_logbox(&logbox);
+      logbox_lasthead = logbox.head;
+    }
+
     // draw_layers(&layer_window, &sys);
     // draw_scrollbars(&sys.screen_state);
     // draw_colorpicker(&sys.colors, !sstate.palette_active);
@@ -94,5 +141,7 @@ ENDMAINLOOP:;
   C3D_Fini();
   // exitRomfs();
   gfxExit();
+
+  tui_logbox_free(&logbox);
   return 0;
 }
